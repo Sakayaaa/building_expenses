a7c03333ef8c32eb042fab51a6a9d71b98e85189
Amirhosein Sebghati <amir.sebghati85@gmail.com>
2025-02-05 17:40:53 +0330

    Delete view directory

--- view/component/__init__.py
+++
@@ -1,5 +0,0 @@
-from view.component.table import Table
-from view.component.label_and_entry import LabelAndEntry
-from view.component.tk_button import TkButton
-from tkinter import *
-import tkinter.messagebox as msg
